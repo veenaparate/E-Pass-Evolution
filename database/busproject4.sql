f55c25c9cf2e6ee7f9724af2c8c2e2879db374b2
veenaparate <120020040+veenaparate@users.noreply.github.com>
2022-12-31 23:51:55 +0530

    Add files via upload

---
+++ database/busproject4.sql
@@ -0,0 +1,360 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2021 at 01:14 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `busproject4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(500) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(500) NOT NULL,
+  `gender` varchar(500) NOT NULL,
+  `dob` text NOT NULL,
+  `contact` text NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `image` varchar(2000) NOT NULL,
+  `created_on` date NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `fname`, `lname`, `gender`, `dob`, `contact`, `address`, `image`, `created_on`, `group_id`) VALUES
+(1, 'admin', 'paratevu@gmail.com', '4bda0284a9632e80fd94500ec09f3164b867ab6a5d78f6a0b038b04f21753708', 'Veena', 'Parate', 'Female', '1988-05-29', '9423979339', 'Nashik', 'pp.jfif', '2018-04-30', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applypass`
+--
+
+CREATE TABLE `applypass` (
+  `id` int(11) NOT NULL,
+  `studentid` int(11) NOT NULL,
+  `route` varchar(150) NOT NULL,
+  `stopfromto` varchar(150) NOT NULL,
+  `durationfees` varchar(150) NOT NULL,
+  `currentdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `startdate` date NOT NULL,
+  `enddate` date NOT NULL,
+  `request` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_website`
+--
+
+CREATE TABLE `manage_website` (
+  `id` int(11) NOT NULL,
+  `title` varchar(600) NOT NULL,
+  `short_title` varchar(600) NOT NULL,
+  `logo` text NOT NULL,
+  `footer` text NOT NULL,
+  `currency_code` varchar(600) NOT NULL,
+  `currency_symbol` varchar(600) NOT NULL,
+  `login_logo` text NOT NULL,
+  `invoice_logo` text NOT NULL,
+  `background_login_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `manage_website`
+--
+
+INSERT INTO `manage_website` (`id`, `title`, `short_title`, `logo`, `footer`, `currency_code`, `currency_symbol`, `login_logo`, `invoice_logo`, `background_login_image`) VALUES
+(1, 'Online Bus Pass Management System Project', 'OBPMS', 'images.jpg', 'Online Bus Pass Management System Project', 'INR', 'â‚¹', 'images.jpg', '', 'background-form-login-1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `route`
+--
+
+CREATE TABLE `route` (
+  `id` int(11) NOT NULL,
+  `start` varchar(150) NOT NULL,
+  `end` varchar(150) NOT NULL,
+  `astatus` varchar(150) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tariff`
+--
+
+CREATE TABLE `tariff` (
+  `id` int(11) NOT NULL,
+  `route` varchar(150) NOT NULL,
+  `stopfromto` varchar(150) NOT NULL,
+  `monthly` int(15) NOT NULL,
+  `quartly` int(15) NOT NULL,
+  `halfyearly` int(15) NOT NULL,
+  `yearly` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_group`
+--
+
+CREATE TABLE `tbl_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_group`
+--
+
+INSERT INTO `tbl_group` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_permission`
+--
+
+CREATE TABLE `tbl_permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `display_name` varchar(200) NOT NULL,
+  `operation` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_permission`
+--
+
+INSERT INTO `tbl_permission` (`id`, `name`, `display_name`, `operation`) VALUES
+(1, 'manage_student', 'Manage Student', 'manage_student'),
+(2, 'add_student', 'Add Student', 'add_student'),
+(3, 'edit_student', 'Edit Student', 'edit_student'),
+(4, 'delete_student', 'Delete Student', 'delete_student'),
+(21, 'manage_user', 'Manage User', 'manage_user'),
+(22, 'add_user', 'Add User', 'add_user'),
+(23, 'edit_user', 'Edit User', 'edit_user'),
+(24, 'delete_user', 'Delete User', 'delete_user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_permission_role`
+--
+
+CREATE TABLE `tbl_permission_role` (
+  `id` int(30) NOT NULL,
+  `permission_id` int(30) NOT NULL,
+  `role_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_permission_role`
+--
+
+INSERT INTO `tbl_permission_role` (`id`, `permission_id`, `role_id`) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 4, 1),
+(5, 5, 1),
+(6, 6, 1),
+(7, 7, 1),
+(8, 8, 1),
+(9, 9, 1),
+(10, 10, 1),
+(11, 11, 1),
+(12, 12, 1),
+(13, 13, 1),
+(14, 14, 1),
+(15, 15, 1),
+(16, 16, 1),
+(17, 17, 1),
+(18, 18, 1),
+(19, 19, 1),
+(20, 20, 1),
+(21, 21, 1),
+(22, 22, 1),
+(23, 23, 1),
+(24, 24, 1),
+(27, 25, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `id` int(11) NOT NULL,
+  `stud_id` varchar(50) NOT NULL,
+  `regno` int(11) NOT NULL,
+  `sfname` varchar(30) NOT NULL,
+  `slname` varchar(30) NOT NULL,
+  `semail` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `sgender` varchar(30) NOT NULL,
+  `sdob` date NOT NULL,
+  `scontact` int(50) NOT NULL,
+  `saddress` varchar(50) NOT NULL,
+  `session` int(11) NOT NULL,
+  `image` varchar(150) NOT NULL,
+  `image1` varchar(150) NOT NULL,
+  `image2` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `applypass`
+--
+ALTER TABLE `applypass`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manage_website`
+--
+ALTER TABLE `manage_website`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `route`
+--
+ALTER TABLE `route`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tariff`
+--
+ALTER TABLE `tariff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_group`
+--
+ALTER TABLE `tbl_group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_permission`
+--
+ALTER TABLE `tbl_permission`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_permission_role`
+--
+ALTER TABLE `tbl_permission_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `applypass`
+--
+ALTER TABLE `applypass`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `manage_website`
+--
+ALTER TABLE `manage_website`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `route`
+--
+ALTER TABLE `route`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tariff`
+--
+ALTER TABLE `tariff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_group`
+--
+ALTER TABLE `tbl_group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tbl_permission`
+--
+ALTER TABLE `tbl_permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `tbl_permission_role`
+--
+ALTER TABLE `tbl_permission_role`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
